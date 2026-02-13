05c1eb5ff419580ee63785018845efcc7a3c390f
Diego <diego@vexandco.com>
2026-02-13 22:38:04 +0000

    Fix Safari login: configure secure cookies with SameSite=lax

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -91,6 +91,34 @@ export const authOptions: NextAuthOptions = {
       return session;
     }
   },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+    csrfToken: {
+      name: `__Host-next-auth.csrf-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
   pages: {
     signIn: "/login",
     error: "/login"
